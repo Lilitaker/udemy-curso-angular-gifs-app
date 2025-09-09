d551a524081178405b19634fb067d75bfe8372cc
Lilitaker <lilitakerloki@gmail.com>
2025-09-09 13:42:17 -0500

    remove old key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@ export const environment = {
   companySlogan: 'Manage your gifs',
 
   // ApiKeys
-  giphyApiKey: 'n6FpFAir4D54xdHyoZ8rkkBmjRrO6COw',
-  giphyUrl: 'https://api.giphy.com/v1',
+  giphyApiKey: '',
+  giphyUrl: '',
 
   // URLs
   
